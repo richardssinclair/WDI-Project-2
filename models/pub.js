3f107695d986d37c00ec08e3d542f26abe526e0d
richardssinclair <richardssinclair@gmail.com>
2017-01-16 18:02:23 +0000

    Autocomplete and directions working

--- models/pub.js
+++ models/pub.js
@@ -7,6 +7,10 @@ const pubSchema = mongoose.Schema({
   location:    {type: String, trim: true, required: true},
   lat:         {type: String, trim: true, required: true},
   lng:         {type: String, trim: true, required: true}
+  // loc: {
+  //   type: [Number],  // [<longitude>, <latitude>]
+  //   index: '2d'      // create the geospatial index
+  // }
 });
 
 module.exports = mongoose.model('Pub', pubSchema);
--- src/js/app.js
+++ src/js/app.js
@@ -25,10 +25,10 @@ App.addInfoWindowForPub = function(pub, marker) {
     this.infowindow = new google.maps.InfoWindow({
       content: `
       <div class="infowindow">
-        <img class="pubImage" src="${ pub.image }">
-        <h3> ${pub.name } </h3>
-        <p> ${ pub.description } </p>
-        <p> ${ pub.location } </p>
+      <img class="pubImage" src="${ pub.image }">
+      <h3> ${pub.name } </h3>
+      <p> ${ pub.description } </p>
+      <p> ${ pub.location } </p>
       </div>`,
       maxWidth: 300
     });
@@ -78,7 +78,7 @@ App.createMarkerForPub = function(pub) {
 
   const beerIcon = {
     url: 'images/beer.png',
-    scaledSize: new google.maps.Size(30, 45),
+    scaledSize: new google.maps.Size(40, 55),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(0, 0)
   };
@@ -108,12 +108,7 @@ App.addPub = function(){
 
 App.getPubs = function() {
   App.ajaxRequest('http://localhost:3000/api/pubs', 'GET', null, App.loopThroughPubs);
-
-  // $.ajax({'http://localhost:3000/api/pubs'}).done(pubs => {
-  //   App.loopThroughPubs(pubs);
-  // });
 };
-//MAP MAP MAP MAP MAP creaiting a map with the id of canvas with map option settings
 
 App.createMap = function(){
   const canvas = document.getElementById('canvas');
@@ -233,206 +228,297 @@ App.createMap = function(){
   this.getCurrentLocation(),
   App.getPubs();
   ////
-  this.directionsDisplay.setMap();
+  // this.directionsDisplay.setMap();
   //////////
+
+  new AutocompleteDirectionsHandler(App.map);
 };
 
 App.loggedInState = function(){
   $('.loggedIn').show();
   $('.loggedOut').hide();
   this.$main.html(`
+    <input id="origin-input" class="controls" type="text"
+    placeholder="Enter an origin location">
+
+    <input id="destination-input" class="controls" type="text"
+    placeholder="Enter a destination location">
+    
+    </div>
     <div id="canvas"></div>
     `);
-  this.createMap();
-};
+    this.createMap();
+  };
 
-App.loggedOutState = function(){
-  $('.loggedIn').hide();
-  $('.loggedOut').show();
-  this.register();
-};
+  App.loggedOutState = function(){
+    $('.loggedIn').hide();
+    $('.loggedOut').show();
+    this.register();
+  };
 
-//register finction, displays register inputs so users gan register
-App.register = function(e){
-  if (e) e.preventDefault();
-  this.$main.html(`
-<form method="post" action="/register">
-  <div class="navbar">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="text-center">Welcome</h1>
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="text" name="user[username]" placeholder="Username">
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="email" name="user[email]" placeholder="Email">
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="password" name="user[password]" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
-          </div>
-          <input class="btn btn-primary" type="submit" value="Register">
-           </div>
-      </div>
-   </div>
-</form>
-  `);
-};
-
-// //
-//     <form method="post" action="/register">
-//       <div class="form-group">
-//         <input class="form-control" type="text" name="user[username]" placeholder="Username">
-//       </div>
-//       <div class="form-group">
-//         <input class="form-control" type="email" name="user[email]" placeholder="Email">
-//       </div>
-//       <div class="form-group">
-//         <input class="form-control" type="password" name="user[password]" placeholder="Password">
-//       </div>
-//       <div class="form-group">
-//         <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
-//       </div>
-//       <input class="btn btn-primary" type="submit" value="Register">
-//     </form>
-//
-// /////////////
-
-//login function so users can log in to the map
-App.login = function(e) {
-  e.preventDefault();
-  this.$main.html(`
-  <form method="post" action="/login">
-    <div class="navbar">
+  //register finction, displays register inputs so users gan register
+  App.register = function(e){
+    if (e) e.preventDefault();
+    this.$main.html(`
+      <form method="post" action="/register">
+      <div class="navbar">
       <div class="modal-content">
+      <div class="modal-header">
+      <h1 class="text-center">Welcome</h1>
+      </div>
+      <div class="form-group">
+      <input class="form-control" type="text" name="user[username]" placeholder="Username">
+      </div>
+      <div class="form-group">
+      <input class="form-control" type="email" name="user[email]" placeholder="Email">
+      </div>
+      <div class="form-group">
+      <input class="form-control" type="password" name="user[password]" placeholder="Password">
+      </div>
+      <div class="form-group">
+      <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
+      </div>
+      <input class="btn btn-primary" type="submit" value="Register">
+      </div>
+      </div>
+      </div>
+      </form>
+      `);
+    };
+
+    // //
+    //     <form method="post" action="/register">
+    //       <div class="form-group">
+    //         <input class="form-control" type="text" name="user[username]" placeholder="Username">
+    //       </div>
+    //       <div class="form-group">
+    //         <input class="form-control" type="email" name="user[email]" placeholder="Email">
+    //       </div>
+    //       <div class="form-group">
+    //         <input class="form-control" type="password" name="user[password]" placeholder="Password">
+    //       </div>
+    //       <div class="form-group">
+    //         <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
+    //       </div>
+    //       <input class="btn btn-primary" type="submit" value="Register">
+    //     </form>
+    //
+    // /////////////
+
+    //login function so users can log in to the map
+    App.login = function(e) {
+      e.preventDefault();
+      this.$main.html(`
+        <form method="post" action="/login">
+        <div class="navbar">
+        <div class="modal-content">
         <div class="modal-header">
-          <h1 class="text-center">Welcome</h1>
-            </div>
-             <div class="modal-body">
-                <div class="form-group">
-                  <input class="form-control" type="email" name="email" placeholder="Email">
-                 </div>
-                 <div class="form-group">
-                    <input class="form-control" type="password" name="password" placeholder="Password">
-                 </div>
-                 <div class="form-group">
-                     <input class="btn btn-primary" type="submit" value="Login">
-                 </div>
-             </div>
+        <h1 class="text-center">Welcome</h1>
         </div>
-     </div>
-  </form>
-  `);
-};
+        <div class="modal-body">
+        <div class="form-group">
+        <input class="form-control" type="email" name="email" placeholder="Email">
+        </div>
+        <div class="form-group">
+        <input class="form-control" type="password" name="password" placeholder="Password">
+        </div>
+        <div class="form-group">
+        <input class="btn btn-primary" type="submit" value="Login">
+        </div>
+        </div>
+        </div>
+        </div>
+        </form>
+        `);
+      };
 
-App.logout = function(e){
-  e.preventDefault();
-  this.removeToken();
-  this.loggedOutState();
-};
+      App.logout = function(e){
+        e.preventDefault();
+        this.removeToken();
+        this.loggedOutState();
+      };
 
-App.homepage = function(){
-  console.log('shabba!');
-};
+      App.homepage = function(){
+        console.log('shabba!');
+      };
 
-App.handleForm = function(e){
-  console.log('running');
-  e.preventDefault();
-  const url    = `${App.apiUrl}${$(this).attr('action')}`;
-  const method = $(this).attr('method');
-  const data   = $(this).serialize();
+      App.handleForm = function(e){
+        console.log('running');
+        e.preventDefault();
+        const url    = `${App.apiUrl}${$(this).attr('action')}`;
+        const method = $(this).attr('method');
+        const data   = $(this).serialize();
 
-  console.log(url, method, data);
-  return App.ajaxRequest(url, method, data, data => {
-    if (data.token) App.setToken(data.token);
-    App.loggedInState();
-  });
-};
+        console.log(url, method, data);
+        return App.ajaxRequest(url, method, data, data => {
+          if (data.token) App.setToken(data.token);
+          App.loggedInState();
+        });
+      };
 
-App.ajaxRequest = function(url, method, data, callback){
-  return $.ajax({
-    url,
-    method,
-    data,
-    beforeSend: this.setRequestHeader.bind(this)
-  })
-  .done(callback)
-  .fail(data => {
-    console.log(data);
-  });
-};
+      App.ajaxRequest = function(url, method, data, callback){
+        return $.ajax({
+          url,
+          method,
+          data,
+          beforeSend: this.setRequestHeader.bind(this)
+        })
+        .done(callback)
+        .fail(data => {
+          console.log(data);
+        });
+      };
 
-App.setRequestHeader = function(xhr) {
-  return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
-};
+      App.setRequestHeader = function(xhr) {
+        return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
+      };
 
-App.setToken = function(token){
-  return window.localStorage.setItem('token', token);
-};
+      App.setToken = function(token){
+        return window.localStorage.setItem('token', token);
+      };
 
-App.getToken = function(){
-  return window.localStorage.getItem('token');
-};
+      App.getToken = function(){
+        return window.localStorage.getItem('token');
+      };
 
-App.removeToken = function(){
-  return window.localStorage.clear();
-};
+      App.removeToken = function(){
+        return window.localStorage.clear();
+      };
 
-$(App.init.bind(App));
+      $(App.init.bind(App));
 
 
 
-//     ///////// GEOLOCATION
-//     directionsService = new google.maps.DirectionsService,
-//     directionsDisplay = new google.maps.DirectionsRenderer
-//     //////////////////////
-//
-//     /////////////
-//
-//     document.getElementById('submit').addEventListener('click', function() {
-//       calculateAndDisplayRoute(directionsService, directionsDisplay);
-//     });
-//   }
-//
-//   function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-//     var waypts = [];
-//     var checkboxArray = document.getElementById('waypoints');
-//     for (var i = 0; i < checkboxArray.length; i++) {
-//       if (checkboxArray.options[i].selected) {
-//         waypts.push({
-//           location: checkboxArray[i].value,
-//           stopover: true
-//         });
-//       }
-//     }
-//
-//     directionsService.route({
-//       origin: document.getElementById('start').value,
-//       destination: document.getElementById('end').value,
-//       waypoints: waypts,
-//       optimizeWaypoints: true,
-//       travelMode: 'WALKING'
-//     }, function(response, status) {
-//       if (status === 'OK') {
-//         directionsDisplay.setDirections(response);
-//         var route = response.routes[0];
-//         var summaryPanel = document.getElementById('directions-panel');
-//         summaryPanel.innerHTML = '';
-//         // For each route, display summary information.
-//         for (var i = 0; i < route.legs.length; i++) {
-//           var routeSegment = i + 1;
-//           summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-//               '</b><br>';
-//           summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-//           summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-//           summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-//         }
-//       } else {
-//         window.alert('Directions request failed due to ' + status);
-//       }
-//     });
-//   }
-// /////////////////////////////////
+      //     ///////// GEOLOCATION
+      //     directionsService = new google.maps.DirectionsService,
+      //     directionsDisplay = new google.maps.DirectionsRenderer
+      //     //////////////////////
+      //
+      //     /////////////
+      //
+      //     document.getElementById('submit').addEventListener('click', function() {
+      //       calculateAndDisplayRoute(directionsService, directionsDisplay);
+      //     });
+      //   }
+      //
+      //   function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      //     var waypts = [];
+      //     var checkboxArray = document.getElementById('waypoints');
+      //     for (var i = 0; i < checkboxArray.length; i++) {
+      //       if (checkboxArray.options[i].selected) {
+      //         waypts.push({
+      //           location: checkboxArray[i].value,
+      //           stopover: true
+      //         });
+      //       }
+      //     }
+      //
+      //     directionsService.route({
+      //       origin: document.getElementById('start').value,
+      //       destination: document.getElementById('end').value,
+      //       waypoints: waypts,
+      //       optimizeWaypoints: true,
+      //       travelMode: 'WALKING'
+      //     }, function(response, status) {
+      //       if (status === 'OK') {
+      //         directionsDisplay.setDirections(response);
+      //         var route = response.routes[0];
+      //         var summaryPanel = document.getElementById('directions-panel');
+      //         summaryPanel.innerHTML = '';
+      //         // For each route, display summary information.
+      //         for (var i = 0; i < route.legs.length; i++) {
+      //           var routeSegment = i + 1;
+      //           summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+      //               '</b><br>';
+      //           summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+      //           summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+      //           summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+      //         }
+      //       } else {
+      //         window.alert('Directions request failed due to ' + status);
+      //       }
+      //     });
+      //   }
+      // /////////////////////////////////
+
+      function AutocompleteDirectionsHandler(map) {
+        this.map = map;
+        this.originPlaceId = null;
+        this.destinationPlaceId = null;
+        this.travelMode = 'WALKING';
+        var originInput = document.getElementById('origin-input');
+        var destinationInput = document.getElementById('destination-input');
+        var modeSelector = document.getElementById('mode-selector');
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        this.directionsDisplay.setMap(map);
+
+        var originAutocomplete = new google.maps.places.Autocomplete(
+          originInput, {placeIdOnly: true});
+          var destinationAutocomplete = new google.maps.places.Autocomplete(
+            destinationInput, {placeIdOnly: true});
+
+            // this.setupClickListener('changemode-walking', 'WALKING');
+            // this.setupClickListener('changemode-transit', 'TRANSIT');
+            // this.setupClickListener('changemode-driving', 'DRIVING');
+
+            this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+            this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+          }
+
+          // Sets a listener on a radio button to change the filter type on Places
+          // Autocomplete.
+          AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+            var radioButton = document.getElementById(id);
+            var me = this;
+            radioButton.addEventListener('click', function() {
+              // me.travelMode = mode;
+              me.route();
+            });
+          };
+
+          AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+            var me = this;
+            autocomplete.bindTo('bounds', this.map);
+            autocomplete.addListener('place_changed', function() {
+              var place = autocomplete.getPlace();
+              if (!place.place_id) {
+                window.alert("Please select an option from the dropdown list.");
+                return;
+              }
+              if (mode === 'ORIG') {
+                me.originPlaceId = place.place_id;
+              } else {
+                me.destinationPlaceId = place.place_id;
+              }
+              me.route();
+            });
+
+          };
+
+          AutocompleteDirectionsHandler.prototype.route = function() {
+            if (!this.originPlaceId || !this.destinationPlaceId) {
+              return;
+            }
+            var me = this;
+
+            var waypts = [];
+
+            this.directionsService.route({
+              origin: {'placeId': this.originPlaceId},
+              destination: {'placeId': this.destinationPlaceId},
+              travelMode: 'WALKING',
+              // waypoints: waypts,
+              // optimizeWaypoints: true
+            }, function(response, status) {
+              if (status === 'OK') {
+                me.directionsDisplay.setDirections(response);
+              } else {
+                window.alert('Directions request failed due to ' + status);
+              }
+            });
+          };
