604333cc9bbc26af92c9c989e96cad818270c5d7
richardssinclair <richardssinclair@gmail.com>
2017-01-17 23:09:34 +0000

    adding pubs

--- db/seeds.js
+++ db/seeds.js
@@ -12,7 +12,7 @@ const pubs = [{
   description: 'the first pint',
   location: 'Broadway Market',
   loc: [-0.061719,51.536365]
-  
+
 },{
   name: 'The Cat & Mutton',
   image: 'https://www.we-heart.com/upload-images/thecatmuttonlondon7.jpg',
@@ -67,56 +67,42 @@ const pubs = [{
   description: 'Jack the ripper pub...',
   location: 'London E1 6QQ',
   loc: [-0.074321,51.519344]
-  // lat: '51.519344',
-  // lng: '-0.074321'
 },{
   name: 'Bagle Bake',
   image: 'http://www.femalefoodie.com/wp-content/uploads/2016/03/image-14-1024x683.jpg',
   description: 'Food Stop....',
   location: '4 Bradbury St, London N16 8JN',
   loc: [-0.071743,51.524525]
-  // lat: '51.524525',
-  // lng: '-0.071743'
 },{
   name: 'Blues Kitchen Shoreditch',
   image: 'http://media.fyre.co/Ke5fXNWBT2mvubCQpOrY_DSC_1103.JPG',
   description: 'blues kitchen in the ditch of shore...',
   location: 'Shoreditch',
   loc: [-0.080173,51.526524]
-  // lat: '51.526524',
-  // lng: '-0.080173'
 },{
   name: 'Bar and Co',
   image: 'https://scontent-a.cdninstagram.com/hphotos-xap1/t51.2885-15/1597218_203007176574328_806588239_s.jpg',
   description: 'Portuguese beer and sandwich.. bring on the meat....',
   location: 'Pratt street, Camden',
   loc: [-0.139471,51.537874]
-  // lat: '51.537874',
-  // lng: '-0.139471'
 },{
   name: 'Camden Head',
   image: 'https://pbs.twimg.com/profile_images/582575219686137857/YkL6EeZX.jpg',
   description: 'avoid...',
   location: 'Camden',
   loc: [-0.140995,51.537265]
-  // lat: '51.537265',
-  // lng: '-0.140995'
 },{
   name: 'The Worlds End',
   image: 'http://www.true-london.com/wp-content/uploads/IMG_0387-e1362658334598.jpg',
   description: 'Strange place',
   location: 'Camden Town',
   loc: [-0.142261,51.539119]
-  // lat: '51.539119',
-  // lng: '-0.142261'
 },{
   name: 'The Camden Eye',
   image: 'http://www.by-invitation.com/wp-content/uploads/2013/01/The-Camden-Eye-Pub.jpg',
   description: 'Open mic mondays.',
   location: 'Camden',
   loc: [-0.142239,51.539462]
-  // lat: '51.539462',
-  // lng: '-0.142239'
 },{
   name: 'The Dublin Castle',
   image:
@@ -124,48 +110,72 @@ const pubs = [{
   description: 'Open mic teusdays',
   location: 'Camden',
   loc: [-0.145550,51.537432]
-  // lat: '51.537432',
-  // lng: '-0.145550'
 },{
   name: 'The Elephants Head',
   image: 'http://www.coolplaces.co.uk/system/images/4930/the-elephants-head-eat-drink-bars-pubs-large.jpg',
   description: 'open mic wednsdays.. nice pub',
   location: 'Camden High Street',
   loc: [-0.143976,51.540713]
-  // lat: '51.540713',
-  // lng: '-0.143976'
 },{
   name: 'The Lock Tavern',
   image: 'http://mrfogg.co.uk/wp-content/uploads/2009/10/img_9589.jpg',
   description: 'good gigs.. nice pub',
   location: 'Near Camden Market',
   loc: [-0.147687,51.542811]
-  // lat: '51.542811',
-  // lng: '-0.147687'
 },{
   name: 'The Monarch',
   image: 'http://monarchbar.com/wp-content/uploads/2016/06/monarch-gallery-7.jpg',
   description: 'Pool, decent gigs..',
   location: 'Near Camden Market',
   loc: [-0.148429,51.542897]
-  // lat: '51.542897',
-  // lng: '-0.148429'
 },{
   name: 'Camden Assembly',
   image: 'http://mixmag.net/assets/uploads/images/_columns2/camden-assembly.jpg',
   description: 'good gigs.. nice pub',
   location: 'Near Camden Market',
   loc: [-0.149224,51.543059]
-  // lat: '51.543059',
-  // lng: '-0.149224'
 },{
   name: 'Quinns',
   image: 'http://www.kentishtowner.co.uk/wp-content/uploads/2012/01/quinnbluesky-e1393864498579.jpg',
   description: 'strange place allways open...',
   location: 'Camden/Kentish Town',
   loc: [-0.141994,51.542838]
-  // lat: '51.542838',
-  // lng: '-0.141994'
+},{
+  name: 'Comercial Tavern',
+  image: 'https://hangthecliche.files.wordpress.com/2014/06/commercial-tavern-spitalfields.jpg',
+  description: 'on the corner.. never been..could be good...?',
+  location: 'near Shoreditch high street',
+  loc: [-0.075669,51.521608]
+},{
+  name: 'The Golden Heart',
+  image: 'https://c2.staticflickr.com/8/7146/6823827533_e8dba64b91_b.jpg',
+  description: 'add description',
+  location: 'Spitafields',
+  loc: [-0.074258,51.520128]
+},{
+  name: 'The Angel',
+  image: 'http://www.fancyapint.com/media/pubimages/pic502.jpg',
+  description: 'The angel pub in angel',
+  location: 'Angel',
+  loc: [-0.106345,51.532171]
+},{
+  name: '02 Academy Islington',
+  image: 'jpg...........',
+  description: 'nice venue for music',
+  location: 'Angel',
+  loc: [-0.105847,51.534329]
+},{
+  name: '............',
+  image: 'jpg...........',
+  description: '.............',
+  location: '.............',
+  loc: [-0.075669,51.521608]
+},{
+  name: '............',
+  image: 'jpg...........',
+  description: '.............',
+  location: '.............',
+  loc: [-0.075669,51.521608]
 }
 ];
 
